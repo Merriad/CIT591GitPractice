51cb8d11ac4003fd91b2cadade541f6956f2251b
Merriad <49375740+Merriad@users.noreply.github.com>
2019-04-14 00:31:42 -0400

    Adding BadPoem.java

---
+++ BadPoem.java
@@ -0,0 +1,8 @@
+
+public class BadPoem {
+	public static void main(String[] args) {
+		System.out.println("Roses are grey.");
+		System.out.println("Violets are grey.");
+		System.out.println("I am a dog.");
+	}
+}
