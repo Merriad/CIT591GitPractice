62283309a223683fcb480181d80131b9722ab5f9
Merriad <49375740+Merriad@users.noreply.github.com>
2019-04-14 20:16:11 -0400

    Changing to blue because dogs can see blue.

--- BadPoem.java
+++ BadPoem.java
@@ -2,7 +2,7 @@
 public class BadPoem {
 	public static void main(String[] args) {
 		System.out.println("Roses are grey.");
-		System.out.println("Violets are grey.");
+		System.out.println("Violets are blue.");
 		System.out.println("I am a dog.");
 	}
 }
